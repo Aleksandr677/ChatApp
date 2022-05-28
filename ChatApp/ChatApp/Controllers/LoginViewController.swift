a2cc7f8afb36b03651fb320659411838db02a1c3
Aleksandr Khristichenko <alexanderhristichenko@yandex.ru>
2022-05-28 15:43:16 +0300

    feat: log out a user

--- ChatApp/ChatApp/Controllers/LoginViewController.swift
+++ ChatApp/ChatApp/Controllers/LoginViewController.swift
@@ -15,6 +15,11 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "⚡️FlashChat"
+    }
+    
     @IBAction func loginPressed(_ sender: UIButton) {
         if let email = emailTextfield.text, let password = passwordTextfield.text {
             Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
--- ChatApp/ChatApp/Controllers/RegisterViewController.swift
+++ ChatApp/ChatApp/Controllers/RegisterViewController.swift
@@ -17,7 +17,7 @@ class RegisterViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        title = "⚡️FlashChat"
     }
     
     @IBAction func registerPressed(_ sender: UIButton) {
