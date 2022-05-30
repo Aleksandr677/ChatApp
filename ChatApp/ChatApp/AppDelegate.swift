016bd18f88ac75c96387f39b6916bde2b5991933
Aleksandr Khristichenko <alexanderhristichenko@yandex.ru>
2022-05-30 19:31:04 +0300

    feat: manage the keyboard by using the swift package manager(IQKeyboardManager)

--- ChatApp/ChatApp/AppDelegate.swift
+++ ChatApp/ChatApp/AppDelegate.swift
@@ -9,6 +9,7 @@ import UIKit
 import FirebaseCore
 import FirebaseFirestore
 import FirebaseAuth
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,8 +19,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         FirebaseApp.configure()
-
         let db = Firestore.firestore() //create a firestore
+        
+        IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.enableAutoToolbar = false
+        IQKeyboardManager.shared.shouldResignOnTouchOutside = true
         
         return true
     }
