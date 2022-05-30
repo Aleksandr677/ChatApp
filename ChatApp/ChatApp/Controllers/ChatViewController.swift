52e0b015d2e82b9ba5e473a1a8fa11554743387b
Aleksandr Khristichenko <alexanderhristichenko@yandex.ru>
2022-05-30 19:03:51 +0300

    feat: sort data retrived from Firestore

--- ChatApp/ChatApp/Controllers/ChatViewController.swift
+++ ChatApp/ChatApp/Controllers/ChatViewController.swift
@@ -38,7 +38,8 @@ class ChatViewController: UIViewController {
         if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email {
             db.collection(K.FStore.collectionName).document().setData([
                 K.FStore.senderField: messageSender,
-                K.FStore.bodyField: messageBody
+                K.FStore.bodyField: messageBody,
+                K.FStore.dateField: Date().timeIntervalSince1970
             ]) { (error) in
                 if let error = error {
                     print("Error writing document: \(error)")
@@ -79,9 +80,13 @@ extension ChatViewController: UITableViewDataSource {
 
 extension ChatViewController {
     func loadMessages() {
-        messages = []
         
-        db.collection(K.FStore.collectionName).getDocuments { (querySnapshot, error) in
+        db.collection(K.FStore.collectionName)
+            .order(by: K.FStore.dateField)
+            .addSnapshotListener { (querySnapshot, error) in
+            
+            self.messages = []
+            
             if let error = error {
                 print("There was an issue retrieving data from Firestore \(error)")
             } else {
